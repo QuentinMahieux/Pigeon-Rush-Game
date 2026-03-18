a5add587dfa640a68b09543deca7029463985380
Quentin MAHIEUX <q.mahieux@rubika-edu.com>
2026-03-18 13:00:47 -0400

    add léna

--- Assets/Script/Element/Teleporter.cs
+++ Assets/Script/Element/Teleporter.cs
@@ -1,12 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
+    public List<string> tags;
     public Transform exit;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Interactable"))
+        if (tags.Contains(other.tag))
         {
             other.transform.position = exit.position;
         }
